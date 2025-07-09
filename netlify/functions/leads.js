279734b875f71135ea41fd436d322b6dd524b380
Mr UnKnown <vinit.uat@gmail.com>
2025-07-09 15:38:01 +0530

    Update leads.js

--- netlify/functions/leads.js
+++ netlify/functions/leads.js
@@ -1,32 +1,40 @@
-// netlify/functions/leads.js
 const { MongoClient, ObjectId } = require('mongodb');
 
-exports.handler = async function (event, context) {
-  const client = new MongoClient(process.env.MONGO_URI);
-  await client.connect();
-  const db = client.db();
-  const leads = db.collection("leads");
-
-  if (event.httpMethod === "GET") {
-    const result = await leads.find({}).toArray();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result)
-    };
+let client;
+async function getCollections() {
+  if (!client) {
+    client = new MongoClient(process.env.MONGO_URI);
+    await client.connect();
   }
+  const db = client.db(process.env.DB_NAME);
+  return db.collection(process.env.COLLECTION);
+}
 
-  if (event.httpMethod === "PUT") {
-    const id = event.path.split("/").pop();
-    const body = JSON.parse(event.body);
-    await leads.updateOne({ _id: new ObjectId(id) }, { $set: body });
-    return { statusCode: 200, body: JSON.stringify({ success: true }) };
+exports.handler = async (event) => {
+  const leads = await getCollections();
+
+  const id = event.path.split('/').pop();
+
+  try {
+    if (event.httpMethod === 'GET') {
+      const all = await leads.find().toArray();
+      return { statusCode: 200, body: JSON.stringify(all) };
+    }
+
+    if (event.httpMethod === 'PUT') {
+      const updated = JSON.parse(event.body);
+      delete updated._id;
+      await leads.updateOne({ _id: new ObjectId(id) }, { $set: updated });
+      return { statusCode: 200, body: JSON.stringify({ success: true }) };
+    }
+
+    if (event.httpMethod === 'DELETE') {
+      await leads.deleteOne({ _id: new ObjectId(id) });
+      return { statusCode: 200, body: JSON.stringify({ success: true }) };
+    }
+
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
   }
-
-  if (event.httpMethod === "DELETE") {
-    const id = event.path.split("/").pop();
-    await leads.deleteOne({ _id: new ObjectId(id) });
-    return { statusCode: 200, body: JSON.stringify({ success: true }) };
-  }
-
-  return { statusCode: 405, body: "Method Not Allowed" };
 };
